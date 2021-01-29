0a51ef42debbc5266893c59b06294a0df4165669
Jeff Soriano <soriano.jeffm@gmail.com>
2021-01-29 14:45:44 -0800

    Render campaigns and add campaign button

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,27 @@
-import React, { useEffect } from 'react';
+import 'semantic-ui-css/semantic.min.css'
 import factory from '../ethereum/factory';
+import { Card, Button } from 'semantic-ui-react'
 
 export default function CampaignIndex({ campaigns }) {
-    return <div>{campaigns[0]}</div>;
+    function renderCampaigns() {
+        const items = campaigns.map(campaign => {
+            return {
+                header: campaign,
+                description: <a>View Campaign</a>,
+                fluid: true
+            }
+        });
+
+        return <Card.Group items={items} />;
+    }
+
+    return (
+        <div>
+            <h3>Open Campaigns</h3>
+            {renderCampaigns()}
+            <Button content="Create Campaign" icon="add circle" primary />
+        </div>
+    );
 }
 
 CampaignIndex.getInitialProps = async (ctx) => {
