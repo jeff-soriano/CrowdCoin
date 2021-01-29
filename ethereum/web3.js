26cd27454209c3589eec9d0e027754d26c841efb
Jeff Soriano <soriano.jeffm@gmail.com>
2021-01-29 14:07:52 -0800

    Update web3 so that it works with Next server side rendering

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,17 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.ethereum);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+    // We are in the browser and metamask is running
+    web3 = new Web3(window.ethereum);
+} else {
+    // We are on the server OR the user is not running metamask
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/v3/9b3cbaa24bbf4b55909e752e91ed67b8'
+    );
+
+    web3 = new Web3(provider);
+}
 
 export default web3;
