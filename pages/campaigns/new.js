4db3fa9e1c1aa1fa8f51efef911642d2a668abbe
Jeff Soriano <soriano.jeffm@gmail.com>
2021-02-02 10:27:39 -0800

    Add css import to Layout

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Layout from '../../components/Layout';
 
 export default function CampaignNew() {
     return (
-        <h1>New campaign!</h1>
+        <Layout>
+            <h1>New campaign!</h1>
+        </Layout>
     );
 }
