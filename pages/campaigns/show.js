c7194ed80634643461dea3e8e6f798779288e7f4
Jeff Soriano <soriano.jeffm@gmail.com>
2021-02-02 14:54:56 -0800

    Add layout to show page

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Layout from '../../components/Layout';
 
 export default function CampaignShow() {
     return (
-        <h1>Campaign show!</h1>
+        <Layout>
+            <h1>Campaign show!</h1>
+        </Layout>
     );
 }
