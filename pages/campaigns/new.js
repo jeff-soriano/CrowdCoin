f8697c79671bd9c06b5e5684e0be59a219013a16
Jeff Soriano <soriano.jeffm@gmail.com>
2021-02-02 10:30:58 -0800

    Add form to new page

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import { Form, Button } from 'semantic-ui-react';
 
 export default function CampaignNew() {
     return (
         <Layout>
-            <h1>New campaign!</h1>
+            <h3>Create a Campaign</h3>
+
+            <Form>
+                <Form.Field>
+                    <label>Minimum Contribution</label>
+                    <input />
+                </Form.Field>
+                <Button primary>Create Campaign</Button>
+            </Form>
         </Layout>
     );
 }
