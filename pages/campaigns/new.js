52f7d9dbfb1f4a2e4a8c4ebdd56cfd59c0e5e5ef
Jeff Soriano <soriano.jeffm@gmail.com>
2021-02-02 10:58:31 -0800

    Add form to new page

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react';
 import Layout from '../../components/Layout';
 import { Form, Button, Input } from 'semantic-ui-react';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 export default function CampaignNew() {
     const [minContribution, setMinContribution] = useState('');
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const accounts = await web3.eth.getAccounts();
+
+        await factory.methods
+            .createCampaign(minContribution)
+            .send({
+                from: accounts[0]
+            });
+    }
 
     return (
         <Layout>
             <h3>Create a Campaign</h3>
 
-            <Form>
+            <Form onSubmit={onSubmit}>
                 <Form.Field>
                     <label>Minimum Contribution</label>
                     <Input
