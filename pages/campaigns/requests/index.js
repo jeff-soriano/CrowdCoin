2959983133f2389d30f0d428a1c9cad13a95c979
Jeff Soriano <soriano.jeffm@gmail.com>
2021-02-03 11:55:17 -0800

    Add routing for new request page

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
+import { Link } from '../../../routes';
+import Layout from '../../../components/Layout';
 
-export default function RequestIndex() {
+export default function RequestIndex({ address }) {
     return (
-        <h1>Request List</h1>
+        <Layout>
+            <h1>Requests</h1>
+            <Link route={`/campaigns/${address}/requests/new`}>
+                <a>
+                    <Button primary>Add Request</Button>
+                </a>
+            </Link>
+        </Layout>
     );
 };
+
+RequestIndex.getInitialProps = async (ctx) => {
+    const address = ctx.query.address;
+
+    return { address };
+}
