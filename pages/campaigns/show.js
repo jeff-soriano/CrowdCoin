5adf1be6017e0ca298b4590236bb45ae4bf2f7a5
Jeff Soriano <soriano.jeffm@gmail.com>
2021-02-03 09:58:52 -0800

    Add campaign summary to show page props

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
 
 export default function CampaignShow() {
     return (
@@ -7,4 +8,17 @@ export default function CampaignShow() {
             <h1>Campaign show!</h1>
         </Layout>
     );
+}
+
+CampaignShow.getInitialProps = async (ctx) => {
+    const campaign = await Campaign(ctx.query.address);
+    const summary = await campaign.methods.getSummary().call();
+
+    return {
+        minimumContribution: summary[0],
+        balance: summary[1],
+        numRequests: summary[2],
+        numApprovers: summary[3],
+        manager: summary[4]
+    };
 }
