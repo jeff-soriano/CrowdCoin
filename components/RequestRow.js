0b81e31648baa1f7cad4b4c331b190cbb739b55a
Jeff Soriano <soriano.jeffm@gmail.com>
2021-02-03 15:17:03 -0800

    Style render rows

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -7,19 +7,28 @@ export default function RequestRow({ id, request, address, numApprovers }) {
 
     function renderRow() {
         const { Row, Cell } = Table;
+        const readyToFinalize = request.approvalCount > numApprovers / 2;
 
         return (
-            <Row>
+            <Row disabled={request.complete} positive={readyToFinalize && !request.complete}>
                 <Cell>{id}</Cell>
                 <Cell>{request.description}</Cell>
                 <Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
                 <Cell>{request.recipient}</Cell>
                 <Cell>{request.approvalCount}/{numApprovers}</Cell>
                 <Cell>
-                    <Button color='green' basic onClick={onApprove}>Approve</Button>
+                    {request.complete ?
+                        null :
+                        (
+                            <Button color='green' basic onClick={onApprove}>Approve</Button>
+                        )}
                 </Cell>
                 <Cell>
-                    <Button color='teal' basic onClick={onFinalize}>Finalize</Button>
+                    {request.complete ?
+                        null :
+                        (
+                            <Button color='teal' basic onClick={onFinalize}>Finalize</Button>
+                        )}
                 </Cell>
             </Row>
         );
