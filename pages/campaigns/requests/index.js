7c15b7ed6aaa70dbee5617984f5143f6192c4f6b
Jeff Soriano <soriano.jeffm@gmail.com>
2021-02-03 14:29:20 -0800

    Initialize requests in view requests page

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Button } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
+import Campaign from '../../../ethereum/campaign';
 
-export default function RequestIndex({ address }) {
+export default function RequestIndex({ address, requests, numRequests }) {
     return (
         <Layout>
             <h1>Requests</h1>
@@ -18,6 +19,14 @@ export default function RequestIndex({ address }) {
 
 RequestIndex.getInitialProps = async (ctx) => {
     const address = ctx.query.address;
+    const campaign = Campaign(address);
+    const numRequests = await campaign.methods.numRequests().call();
 
-    return { address };
+    const requests = await Promise.all(
+        Array(parseInt(numRequests)).fill().map((element, index) => {
+            return campaign.methods.requests(index).call();
+        })
+    );
+
+    return { address, requests, numRequests };
 }
